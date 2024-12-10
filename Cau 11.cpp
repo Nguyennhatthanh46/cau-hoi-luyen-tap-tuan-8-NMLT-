f942983778e9e7d5339f01dfdee8df475c8248e8
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2024-12-10 21:27:03 +0700

    Mảng 3 chiều (Bài 2)

---
+++ Cau 11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a [2][3][2] ={{ {1, 2},{3, 4},{ 5, 6} }, { {7, 8},{9, 10},{11, 12} }};
+cout<<a[1][1][1];
+return 0;
+}
+//Kết quả: 10. 
