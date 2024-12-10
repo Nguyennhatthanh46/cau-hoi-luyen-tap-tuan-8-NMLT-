5fe07c5a06077a47e8a1346c934ffa01700f98c8
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2024-12-10 22:22:11 +0700

    Mảng chuỗi kí tự (bài 4)

---
+++ Cau 15.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include <string.h>
+using namespace std;
+int main() {
+char s[] = "Chuc cac em thi tot.";
+cout << strlen(s);
+return 0;
+}
+//Kết quả: 10.    Đếm chiều dài của mảng kí tự, gồm 20, tính cả khoản trắng.
