6a8e4d2613e96b7848245e44cf45b08d77fabe37
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2024-12-10 22:26:25 +0700

    Mảng 1 chiều (Bài 3)

---
+++ Cau 16.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+int Arr[3][3]={{1,2,3} ,{2,1,3} ,{3,2,1}};
+cout<<Arr[1][2]<<endl;
+return 0;
+}
+// Kết quả: 3. Truy cập ví trị phần tử. 
