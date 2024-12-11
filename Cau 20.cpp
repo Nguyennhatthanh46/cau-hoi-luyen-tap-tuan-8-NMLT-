b5e16ad9de4473070a88d4cb7ffab1cfd5978402
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2024-12-11 20:59:10 +0700

    Mảng 2 chiều (Bài 6)

---
+++ Cau 20.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a[2][3];
+a[][] = {{1, 2, 3}, {4, 5, 6}};
+cout << a[1][0];
+}
+//Kết quả: Lỗi. a[][] = {{1, 2, 3}, {4, 5, 6}};, không thể sử dụng toán tử gán (=) để gán trực tiếp các giá trị cho một mảng hai chiều đã được khai báo trước. 
